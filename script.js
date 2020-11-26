c0d65c43e38201c39b98933ee0f6a018521d18b5
HEYROO-KOR <cigul12@icloud.com>
2020-11-26 23:22:33 +0900

    .

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const icon = document.querySelector('.icon');
 const on = document.querySelector('.fas.fa-video');
 const off = document.querySelector('.fas.fa-video-slash');
+const camera = document.querySelector('.camera');
 
 icon.addEventListener('click',() => {
     on.classList.toggle('invisible');
     off.classList.toggle('invisible');
 });
+
+const URL = "./model/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(480, 480, flip); // width, height, flip
+    await webcam.setup();
+    // await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    camera.appendChild(webcam.canvas);
+    // labelContainer = document.getElementById("label-container");
+    // for (let i = 0; i < maxPredictions; i++) { // and class labels
+    //    labelContainer.appendChild(document.createElement("div"));
+    // }
+}
